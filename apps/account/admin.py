9c519d1a7ebbf65145791c570a5097a41c4c69b8
danila nikitin <danila.n2015@yandex.ru>
2026-07-05 19:43:38 +0300

    fix: fix account admin and urls

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -6,12 +6,11 @@ from apps.account.models import Account, PrimaryToken
 # Register your models here.
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('username', 'id', 'first_name', 'last_name', 'phone', 'email', 'is_active', )
+    list_display = ('username', 'id', 'first_name', 'last_name', 'email', 'is_active', )
     readonly_fields = ('created', 'updated', )
     fieldsets = (
         (None, {'fields': ('username',)}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'patronymic', 'email',
-                                         'phone', 'birth_date', 'gender')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'patronymic', 'email','gender')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'), }),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Dop', {'fields': ('created', 'updated', )}),
--- settings/urls.py
+++ settings/urls.py
@@ -17,16 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/swagger/'), name='index'),
     path('admin/', admin.site.urls),
-
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
     path('users/', include('apps.account.urls', namespace='account')),
-    path('info_cards/', include('apps.info_cards.urls', namespace='info_cards')),
 ]
